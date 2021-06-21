f37ec72f4974a99e6249765a85add3e6d94b0805
srivatsamc <78139019+srivatsamc@users.noreply.github.com>
2021-06-21 20:12:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+#region Definition of class Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student_Info instance = new Student_Info();
+            instance.Setid("S100");
+            instance.Setname("Tom");
+            instance.Setmarks(70);
+
+            string idvalue = instance.Getid();
+            string namevalue = instance.Getname();
+            int marksvalue = instance.Getmarks();
+
+            System.Console.WriteLine(idvalue);
+            System.Console.WriteLine(namevalue);
+            System.Console.WriteLine(marksvalue);
+        }
+    }
+}
+#endregion
---
+++ StudentInfo .cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+#region Definition of Class
+    class Student_Info
+    {
+#region User defined private data types
+        private string id;
+        private string name;
+        private int marks;
+        #endregion
+
+#region Intializing User defined data types
+        public string Getid()
+        {
+            return this.id;
+        }
+
+        public void Setid(string id)
+        {
+           this.id=id;
+        }
+
+        public string Getname()
+        {
+            return this.name;
+        }
+
+        public void Setname(string name)
+        {
+             this.name=name;
+        }
+
+        public int Getmarks()
+        {
+            return this.marks;
+        }
+
+        public void Setmarks(int marks)
+        {
+            this.marks=marks;
+        }
+#endregion
+    }
+#endregion
+}
